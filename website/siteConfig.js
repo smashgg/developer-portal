664bb0fcf4e750d72129f26a5c691dbe1fc88df5
Mitchell Morley <mmorley@smash.gg>
2018-12-03 18:10:52 -0500

    trying out the codepen css style for code blocks

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -63,7 +63,7 @@ const siteConfig = {
 
 	highlight: {
 		// Highlight.js theme to use for syntax highlighting in code blocks.
-		theme: 'default',
+		theme: 'codepen-embed',
 	},
 
 	// Add custom scripts here that would be placed in <script> tags.
