5c2c468c29371a937472e4a50a65f8087c6a6749
Mitchell Morley <mmorley@smash.gg>
2019-02-08 12:07:41 -0500

    sticking to one section for now. will reinstate OSS section later

--- website/pages/en/users.js
+++ website/pages/en/users.js
@@ -15,10 +15,10 @@ const translate = require('../../server/translate.js').translate;
 class Users extends React.Component {
 	render() {
 		const { config: siteConfig } = this.props;
-		const fbUsersToShowcase = siteConfig.users.filter(
-			user => user.fbOpenSource,
+		const openSourceToShowcase = siteConfig.users.filter(
+			user => user.openSource,
 		);
-		const restToShowcase = siteConfig.users.filter(user => !user.fbOpenSource);
+		const featuredToShowcase = siteConfig.users.filter(user => user.pinned);
 
 		return (
 			<div className="mainContainer">
@@ -26,32 +26,26 @@ class Users extends React.Component {
 					<div className="showcaseSection">
 						<div className="prose">
 							<h1>
-								<translate>Who is using Docusaurus?</translate>
+								Who is using the smash.gg public API?
 							</h1>
 							<p>
-								Docusaurus powers some of Facebook&apos;s popular{' '}
-								<a href="https://code.facebook.com/projects/">
-									open source projects
-                </a>
-								.
-              </p>
+								Data from smash.gg powers some of the esports world's popular projects.
+              				</p>
 						</div>
-						<Showcase users={fbUsersToShowcase} />
-						<div className="prose">
+						<Showcase users={featuredToShowcase} />
+						{/* <div className="prose">
 							<p>
-								<translate>
-									Docusaurus is also used by open source projects of all sizes.
-                </translate>
+									Including open source projects of all sizes.
 							</p>
 						</div>
-						<Showcase users={restToShowcase} />
+						<Showcase users={openSourceToShowcase} /> */}
 						<div className="prose">
 							<p>
-								<translate>Is your project using Docusaurus?</translate>
+								Is your project using smash.gg?
 							</p>
 							<p>
 								Edit this page with a{' '}
-								<a href="https://github.com/facebook/docusaurus/edit/master/v1/website/data/users.js">
+								<a href="https://github.com/smashgg/developer-portal/edit/master/website/data/users.js">
 									Pull Request
                 </a>{' '}
 								to add your logo.
