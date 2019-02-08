c92400d61470421fd823d9cc2e60befa9f5332cc
Mitchell Morley <mmorley@smash.gg>
2019-02-08 13:19:14 -0500

    added brawlhalla matchups to users

--- website/data/users.js
+++ website/data/users.js
@@ -8,10 +8,10 @@
 module.exports = [
 	// Please add your logo in alphabetical order of caption.
 	{
-		caption: 'Twitch Rivals',
-		image: 'https://static-cdn.jtvnw.net/jtv_user_pictures/17ef934c-2fa1-42d3-a278-29fc7f645d5c-profile_image-300x300.jpeg',
-		infoLink: 'https://rivals.twitch.tv',
-		openSource: false,
+		caption: 'Brawlhalla Matchups',
+		image: 'https://imgur.com/rO1rrqv.png',
+		infoLink: 'https://github.com/henryaguerra/brawlhalla-matchups-bot',
+		openSource: true,
 		pinned: true,
 	},
 	{
@@ -20,6 +20,13 @@ module.exports = [
 		infoLink: 'https://chrome.google.com/webstore/detail/smashticker-watch-people/nhjklhalmbccpfhpnedcleiabpkocggi',
 		openSource: true,
 		pinned: true,
-	}
+	},
+	{
+		caption: 'Twitch Rivals',
+		image: 'https://smashgg.imgix.net/images/tournament/121788/image-5b5213207f45a30e8e1c938187f8de3e.jpg?auto=compress,format&w=280&h=280&fit=crop',
+		infoLink: 'https://rivals.twitch.tv',
+		openSource: false,
+		pinned: true,
+	},
 	// Please add your logo in alphabetical order of caption.
 ];
