fad8c41414f7e63689657581dc21b0d0c356ee10
elvyn <45512739+leElvyn@users.noreply.github.com>
2021-01-13 17:33:38 +0100

    Update users.js

--- website/data/users.js
+++ website/data/users.js
@@ -38,6 +38,13 @@ const users = [
     pinned: true,
   },
   {
+    caption: 'Smashthèque',
+    image: '',
+    infoLink: 'https://www.smashtheque.fr/',
+    openSource: true,
+    pinned: true,
+  },
+  {
     caption: 'SmashTicker',
     image:
       'https://lh3.googleusercontent.com/HJzDy4IhHwwpmb_LZtYs4tmth-Z4N8BYltaxBlgMjkIaB97HIer3HJY3V6Ebh39dKn2s0qGRGuo=w128-h128-e365',
